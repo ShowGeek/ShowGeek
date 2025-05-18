20bd86f022c5a5a24de9cd47d38c4f6a525a2b01
Yasir Alsaif <yasir.alsaif@yahoo.se>
2025-05-18 22:06:59 +0200

    Create Navbar.js

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white p-4 flex justify-between">
+      <h1 className="text-xl font-bold">TVShowDB</h1>
+      <button className="bg-blue-600 px-4 py-2 rounded">Login</button>
+    </nav>
+  );
+}
